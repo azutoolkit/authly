e82b8598b896da5f75fb318f043a18c43e6dd92e
Elias Perez <eliasjpr@gmail.com>
2021-10-30 09:01:45 -0400

    Remove useless assignment and renames cid to client_id

--- spec/grants/authorization_code_spec.cr
+++ spec/grants/authorization_code_spec.cr
@@ -3,7 +3,7 @@ require "../spec_helper"
 module Authly
   describe AuthorizationCode do
     code_data = {
-      "cid"          => "1",
+      "client_id"    => "1",
       "secret"       => "secret",
       "redirect_uri" => "https://www.example.com/callback",
       "scope"        => "read",
@@ -11,22 +11,21 @@ module Authly
     }
 
     describe "#authorize" do
-      cid, secret, uri = code_data.values
-      state = Random::Secure.hex
+      client_id, secret, uri = code_data.values
       code_verifier = Faker::Internet.password(43, 128)
 
       describe "code challenge" do
         it "peforms plain code challenge authorization" do
           code_challenge = code_verifier
           code = Code.new(code_challenge, "plain").to_s
-          auth_code = AuthorizationCode.new(cid, secret, uri, code, code_verifier)
+          auth_code = AuthorizationCode.new(client_id, secret, uri, code, code_verifier)
           auth_code.authorized?.should be_truthy
         end
 
         it "peforms S256 code challenge authorization" do
           code_challenge = Digest::SHA256.base64digest(code_verifier)
           code = Code.new(code_challenge, "S256").to_s
-          authorization_code = AuthorizationCode.new(cid, secret, uri, code, code_verifier)
+          authorization_code = AuthorizationCode.new(client_id, secret, uri, code, code_verifier)
 
           authorization_code.authorized?.should be_truthy
         end
@@ -34,13 +33,13 @@ module Authly
 
       it "returns false" do
         code = Code.new.to_s
-        authorization_code = AuthorizationCode.new(cid, secret, uri, code)
+        authorization_code = AuthorizationCode.new(client_id, secret, uri, code)
         authorization_code.authorized?.should be_truthy
       end
 
       it "raises error for invalid client credentials" do
         code = Code.new.to_s
-        authorization_code = AuthorizationCode.new(cid, "invalid", uri, code)
+        authorization_code = AuthorizationCode.new(client_id, "invalid", uri, code)
 
         expect_raises Error, ERROR_MSG[:unauthorized_client] do
           authorization_code.authorized?
@@ -58,7 +57,7 @@ module Authly
 
       it "raises Error for redirect uri" do
         code = Code.new.to_s
-        authorization_code = AuthorizationCode.new(cid, secret, "", code)
+        authorization_code = AuthorizationCode.new(client_id, secret, "", code)
 
         expect_raises Error, ERROR_MSG[:invalid_redirect_uri] do
           authorization_code.authorized?
