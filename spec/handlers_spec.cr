9778fa9d7a1fa98a03b3af133a185fc595bf5480
eliasjpr <eliasjpr@gmail.com>
2024-10-09 18:02:42 -0400

    Remove debugging `puts` statements in specs
    Eliminated unnecessary `puts` statements in the test suite to enhance output cleanliness and maintain focus on assertions. This improves the clarity of test results and aligns with best practices for writing cleaner testing code.

--- spec/handlers_spec.cr
+++ spec/handlers_spec.cr
@@ -7,7 +7,7 @@ module Authly
     it "returns authorization code with valid client_id and redirect_uri" do
       response = HTTP::Client.get("http://127.0.0.1:8080/oauth/authorize?client_id=1&redirect_uri=https://www.example.com/callback&response_type=code")
       body = response.body
-      puts body
+       body
 
       response.status_code.should eq 302
 
@@ -33,7 +33,7 @@ module Authly
       })
       response.status_code.should eq 200
       body = JSON.parse(response.body)
-      puts body["access_token"]
+       body["access_token"]
       body["access_token"].should_not be_nil
     end
 
